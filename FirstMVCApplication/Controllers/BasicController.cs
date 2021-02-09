22d7fb5476a60ed4fadd7aab1edde71a19cc2cd1
Avrumi Friedman <a.friedman07@gmail.com>
2021-02-08 21:33:03 -0500

    Initial commit

---
+++ FirstMVCApplication/Controllers/BasicController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Web;
+using System.Web.Mvc;
+using FirstMVCApplication.Models;
+
+namespace FirstMVCApplication.Controllers
+{
+    public class BasicController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyOtherPage()
+        {
+            return View();
+        }
+    }
+    
+    //create a simple MVC application with the following pages:
+    
+    // /Home/PageOne - this page should link to /home/pagetwo 
+    // /Home/PageTwo - this page should link to /home/pageone
+    
+    // /Other/First - this page should link to goolge
+}
---
+++ FirstMVCApplication/Models/IndexPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMVCApplication.Models
+{
+    public class IndexPageViewModel
+    {
+        public int RandomNumber { get; set; }
+    }
+}
